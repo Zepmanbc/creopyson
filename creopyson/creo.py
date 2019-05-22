312e4b7600f13db75815bbbc5df7065cf4ee3a82
ZepmanBC <zepman@gmail.com>
2019-05-22 11:52:51 +0200

    creo> delete_files

--- creopyson/creo.py
+++ creopyson/creo.py
@@ -32,8 +32,45 @@ def cd(client, dirname):
         raise Warning(data)
 
 
-# def delete_files():
-#     pass
+def delete_files(client, dirname=None, filename=None, filenames=None):
+    """Delete files from a directory working directory.
+
+    Args:
+        client (obj):
+            creopyson Client.
+        dirname (str, optional):
+            Directory name. Defaults is Creo's current working directory.
+        filename (str, optional):
+            File name filter; only used if filenames is not give
+            (wildcards_allowed: True). Defaults to None.
+        filenames (list:str, optional):
+            List of file names. Defaults to None.
+            The filename parameter is used; if both are blank,
+            all files will be deleted.
+
+    Raises:
+        Warning: error message from creoson.
+
+    Returns:
+        (list:str): List of deleted files.
+
+    """
+    request = {
+        "sessionId": client.sessionId,
+        "command": "creo",
+        "function": "dele_files",
+    }
+    if dirname:
+        request["data"]["dirname"] = dirname
+    if filename:
+        request["data"]["filename"] = filename
+    if filenames:
+        request["data"]["filenames"] = filenames
+    status, data = creoson_post(client, request)
+    if not status:
+        return data["filelist"]
+    else:
+        raise Warning(data)
 
 
 # def get_config():
