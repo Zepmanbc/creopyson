1d4b1e06ebdffeb43a89b9f4ea861204bd93fba0
ZepmanBC <zepman@gmail.com>
2019-05-22 10:16:23 +0200

    dimension> list_detail

--- creopyson/dimension.py
+++ creopyson/dimension.py
@@ -101,14 +101,95 @@ def list_(
         request["data"]["encoded"] = encoded
     status, data = creoson_post(client, request)
     if not status:
-        return data
+        return data["dimlist"]
     else:
         raise Warning(data)
     # TODO only 1 entry for name/names
 
 
-# def list_detail():
-#     pass
+def list_detail(
+    client,
+    current_file=None,
+    name=None,
+    names=None,
+    dim_type=None,
+    encoded=None
+):
+    """Get a list of dimension details from a model.
+
+    Args:
+        client (obj):
+            creopyson Client.
+        current_file (str, optional):
+            Model name. Defaults is current active model.
+        name (str, optional):
+            Dimension name; only used if names is not given.
+            Defaults: The name parameter is used; if both are empty,
+            then all dimensions are listed.
+        names (list:str, optional):
+            List of dimension names. Defaults to None.
+        dim_type (str, optional):
+            Dimension type filter. Defaults is `no filter`.
+            Valid values: linear, radial, diameter, angular.
+        encoded (boolean, optional):
+            Whether to return the values Base64-encoded. Defaults is False.
+
+    Raises:
+        Warning: error message from creoson.
+
+    Returns:
+        (list:dict): List of dimension information.
+            name (str):
+                Dimension name
+            value (str|float):
+                Dimension value; if encoded is True it is a str,
+                if encoded is False it is a float.
+            encoded (boolean):
+                Whether the returned value is Base64-encoded.
+            sheet (int):
+                Sheet number.
+            view_name (str):
+                View name.
+            dim_type (str):
+                Dimension type.
+                Valid values: linear, radial, diameter, angular.
+            text (str):
+                dimension text.
+            location (dict): Coordonates location.
+                x (float): X coordonate location.
+                y (float): Y coordonate location.
+                z (float): Z coordonate location.
+            tolerance_type (str):
+                Tolerance type, if not specified not returned.
+                Valid values: plus_minus (TODO complete list).
+            tol_plus (float):
+                Plus tolerance value.
+                if tolerance_type not specified not returned.
+            tol_minus (float):
+                Minus tolerance value.
+                if tolerance_type not specified not returned.
+
+    """
+    request = {
+        "sessionId": client.sessionId,
+        "command": "dimension",
+        "function": "list_detail",
+    }
+    if current_file:
+        request["data"]["file"] = current_file
+    if name:
+        request["data"]["name"] = name
+    if names:
+        request["data"]["names"] = names
+    if dim_type:
+        request["data"]["dim_type"] = dim_type
+    if encoded:
+        request["data"]["encoded"] = encoded
+    status, data = creoson_post(client, request)
+    if not status:
+        return data["dimlist"]
+    else:
+        raise Warning(data)
 
 
 def set_(client, name, value, current_file=None):
