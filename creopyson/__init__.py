e6820f513e6139c1f52d96ebaf19a48d0dba7c79
Zepmanbc <zepman@gmail.com>
2020-07-16 10:17:13 +0200

    Bump version: 0.5.2 → 0.6.0

--- creopyson/__init__.py
+++ creopyson/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Benjamin C."""
 __email__ = "zepman@gmail.com"
-__version__ = "0.5.2"
+__version__ = "__version__ = '0.6.0'"
 
 from creopyson.connection import Client
 from creopyson.objects import jlpoint
